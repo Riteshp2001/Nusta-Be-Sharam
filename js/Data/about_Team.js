7a2e7fa72c2bd87c676c271e27e410c5b1df0fd4
Riteshp2001 <riteshp2001@gmail.com>
2024-11-26 20:29:35 +0530

    Added Everything

--- js/Data/about_Team.js
+++ js/Data/about_Team.js
@@ -1,53 +1,8 @@
 const teamCard = [
     {
         img: "team1.webp",
-        name: "Lily Rose",
+        name: "Kunal Dhullarwar",
         role: "Photographer",
-    },
-    {
-        img: "team2.webp",
-        name: "John Paul",
-        role: "Assistant Photographer",
-    },
-    {
-        img: "team3.webp",
-        name: "Emma Grace",
-        role: "Studio Manager",
-    },
-    {
-        img: "team4.webp",
-        name: "Mark Anthony",
-        role: "Digital Retoucher",
-    },
-    {
-        img: "team5.webp",
-        name: "Mia Belle",
-        role: "Art Director",
-    },
-    {
-        img: "team6.webp",
-        name: "James Dean",
-        role: "Stylist",
-    },
-    {
-        img: "team7.webp",
-        name: "Ava Marie",
-        role: "Production Assistant",
-    },
-    {
-        img: "team8.webp",
-        name: "David Lee",
-        role: "Marketing and Sales",
-    },
-    {
-        img: "team9.webp",
-        name: "Zoe Claire",
-        role: "Graphic Designer",
-    },
-    {
-        img: "team10.webp",
-        name: "Michael Scott",
-        role: "Administrative Staff",
     },
 ];
 
